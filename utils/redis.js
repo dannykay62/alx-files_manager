5443aa9b86bec58201ea4b07a15d1f3e1af9c2d7
dannykay62 <danielkay62@gmail.com>
2024-01-08 23:54:59 +0100

    Created redis.js that contains the class RedisClient for baxic operations

--- utils/redis.js
+++ utils/redis.js
@@ -1,40 +1,70 @@
 import redis from 'redis';
 import { promisify } from 'util';
-
+/**
+ * This class is used to perform operation with Redis service
+ */
 class RedisClient {
   constructor() {
     this.client = redis.createClient();
 
-    // Promisifying Redis methods
+    /* Promisify Redis methods */
     this.getAsync = promisify(this.client.get).bind(this.client);
     this.setexAsync = promisify(this.client.setex).bind(this.client);
     this.delAsync = promisify(this.client.del).bind(this.client);
 
-    // Handling error and connect events
+    /* handle error and connect events */
     this.client.on('error', (error) => {
-      console.log(`Redis client error: ${error.message}`);
+      console.log(`Redis client not connected to the server: ${error.message}`);
     });
     this.client.on('connect', () => {
       console.log('Redis client connected');
     });
   }
 
+
+  /**
+   * checks if connection to Redis is successfull/Alive
+   * @returns true when the connection to Redis is a success otherwise, otherwise false
+   */
   isAlive() {
     return this.client.connected;
   }
 
+  /**
+   * takes a string key as argument and returns the Redis value
+   * stored for this key
+   * @param {string} key string key to search for in Redis
+   * @returns {string} value of key
+   */
   async get(key) {
-    return await this.getAsync(key);
+    const value = await this.getAsync(key);
+    return value;
   }
 
+
+  /**
+   * takes a string key, a value and a duration in second as arguments to
+   * store it in Redis (with an expiration set by the duration argument)
+   * @param {string} key to be stored in Redis
+   * @param {string} value to assign to the key
+   * @param {number} duration TTL, expiration
+   * @returns {undefined} No return
+   */
   async set(key, value, duration) {
     await this.setexAsync(key, duration, value);
   }
 
+
+  /**
+   * takes a string key as argument and remove the value in Redis for this key
+   * @param {string} key to delete
+   * @returns {undefined} No return
+   */
   async del(key) {
     await this.delAsync(key);
   }
 }
 
 const redisClient = new RedisClient();
-export default redisClient;
+
+module.exports = redisClient;
